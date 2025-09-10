598ec3b2e404dc7779c51ced6fe5bae7861d2d8c
Tatyana Avdeeva <tatyana.best@list.ru>
2025-09-10 10:18:22 +0300

    chore(init commit):Init bitrix24 project

---
+++ .bash_history
@@ -0,0 +1,29 @@
+ls
+ssh-keygen
+cd .ssh
+ls
+touch authorized_keys
+nano authorized_keys
+nano authorized_keys
+exit
+ls
+cd ..
+ls
+sudo nano /etc/ssh/sshd_config
+nano /etc/ssh/sshd_config
+ls
+cd ..
+ls
+cd ..
+ls
+cd ..
+ls
+cd ..
+ls
+cd ..
+ls
+cd ..
+ls
+exit
+ls
+exit
---
+++ .vscode-server/cli/servers/Stable-6f17636121051a53c88d3e605c491d22af2ba755/server/bin/helpers/browser.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+ROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+
+APP_NAME="code"
+VERSION="1.103.2"
+COMMIT="6f17636121051a53c88d3e605c491d22af2ba755"
+EXEC_NAME="code"
+CLI_SCRIPT="$ROOT/out/server-cli.js"
+"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
