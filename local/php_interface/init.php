ff5bb923a50b67e5ea8190de7829fc572aeb098e
Tatyana Avdeeva <tatyana.best@list.ru>
2025-09-15 22:56:51 +0700

    Debug and logging

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,5 +1,7 @@
 <?php
 
+use Bitrix\Main\Diag\Debug;
+
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once __DIR__ . '/../vendor/autoload.php';
 }
@@ -7,3 +9,27 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 if (file_exists(__DIR__ . '/events.php')) {
     require_once __DIR__ . '/events.php';
 }
+
+
+spl_autoload_register(function($sClassName)
+{
+    $sClassFile = __DIR__ . '/classes';
+
+    if ( file_exists($sClassFile . '/' . str_replace('\\', '/', $sClassName) . '.php') )
+    {
+        require_once($sClassFile . '/'.str_replace('\\', '/', $sClassName) . '.php');
+        return;
+    }
+
+    $arClass = explode('\\', strtolower($sClassName));
+    foreach($arClass as $sPath )
+    {
+        $sClassFile .= '/' . ucfirst($sPath);
+    }
+
+    $sClassFile .= '.php';
+    if (file_exists($sClassFile))
+    {
+        require_once($sClassFile);
+    }
+});
