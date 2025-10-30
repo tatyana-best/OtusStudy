071891f7d05678a0a96dadf7c8830ab54674c5fa
Tatyana Avdeeva <tatyana.best@list.ru>
2025-10-30 09:31:14 +0300

    Some correct

--- local/modules/crm.tab/install/components/OtusMain/book.grid/templates/.default/template.php
+++ local/modules/crm.tab/install/components/OtusMain/book.grid/templates/.default/template.php
@@ -85,62 +85,6 @@ $APPLICATION->IncludeComponent(
     $component,
 );
 
-$APPLICATION->IncludeComponent(
-    'bitrix:main.ui.grid',
-    '',
-    [
-        'GRID_ID' => $arResult['FILTER_ID'],
-        'HEADERS' => $arResult['HEADERSQ'],
-        'ROWS' => $arResult['GRID_LISTQ'],
-        'TOTAL_ROWS_COUNT' => 5,//$arResult['NAV']->getRecordCount(),
-        'NAV_OBJECT' => $arResult['NAV'],
-        'FILTER_STATUS_NAME' => '',
-        'AJAX_MODE' => 'Y',
-        'AJAX_OPTION_JUMP' => 'N',
-        'AJAX_OPTION_STYLE' => 'N',
-        'AJAX_OPTION_HISTORY' => 'N',
-        'AJAX_LOADER' => $arParams['AJAX_LOADER'],
-        'AJAX_ID' => \CAjax::getComponentID(
-            'bitrix:main.ui.grid',
-            '.default',
-            ''
-        ),
-        'ALLOW_COLUMNS_SORT' => true,
-        'ALLOW_ROWS_SORT' => [],
-        'ALLOW_COLUMNS_RESIZE' => true,
-        'ALLOW_HORIZONTAL_SCROLL' => true,
-        'ALLOW_SORT' => true,
-        'ALLOW_PIN_HEADER' => true,
-        'ACTION_PANEL' => [],
-
-        'SHOW_CHECK_ALL_CHECKBOXES' => false,
-        'SHOW_ROW_CHECKBOXES' => false,
-        'SHOW_ROW_ACTIONS_MENU' => true,
-        'SHOW_GRID_SETTINGS_MENU' => true,
-        'SHOW_NAVIGATION_PANEL' => true,
-        'SHOW_PAGINATION' => true,
-        'SHOW_SELECTED_COUNTER' => true,
-        'SHOW_TOTAL_COUNTER' => true,
-        'SHOW_PAGESIZE' => true,
-        'SHOW_ACTION_PANEL' => true,
-
-        'ENABLE_COLLAPSIBLE_ROWS' => true,
-        'ALLOW_SAVE_ROWS_STATE' => true,
-
-        'SHOW_MORE_BUTTON' => false,
-        'CURRENT_PAGE' => '',
-        'DEFAULT_PAGE_SIZE' => 20,
-        'PAGE_SIZES' => [
-            ['NAME' => 1, 'VALUE' => 1],
-            ['NAME' => 5, 'VALUE' => 5],
-            ['NAME' => 10, 'VALUE' => 10],
-            ['NAME' => 20, 'VALUE' => 20],
-            ['NAME' => 50, 'VALUE' => 50],
-        ],
-    ],
-    $component,
-);
-
 if (!empty($arParams['AJAX_LOADER'])) { ?>
     <script>
         BX.addCustomEvent('Grid::beforeRequest', function (gridData, argse) {
