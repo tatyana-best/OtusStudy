a4e2c929044e2a22d9d036757eaf98248be6fda9
Tatyana Avdeeva <tatyana.best@list.ru>
2025-12-06 07:53:05 +0300

    Start day, attempt any one

--- local/jsLibs/workStart/workStart.js
+++ local/jsLibs/workStart/workStart.js
@@ -28,14 +28,13 @@ BX.ready(function() {
     //onTimeManNeedRebuild, onTimeManDataRecieved, onTaskTimerChange, onTimeManDayOpen
     // А вот это вообще не ловится: onTimeManWindowOpen
 
-    BX.addCustomEvent("onTimeManDataRecieved", function(p, q) {
-        let state = p.STATE;
-        let id = p.ID;
-        console.log(p);
+    BX.addCustomEvent("onTimeManDataRecieved", function(p) {
+        //alert(12345678);
         if ($('.tm-timer__value').length && $('.--play-l').length) {
-            if (p.hasOwnProperty('STATE') && p.STATE == 'OPENED') {
+           if (p.hasOwnProperty('STATE') && p.STATE == 'OPENED') {
+               $('ul.tm-control-panel__actions-list li').hide();
                 var popup = BX.PopupWindowManager.create("popup-message", BX('element'), {
-                    content: '<div style="display:none;"></div>',
+                    content: '',
                     width: 250,
                     height: 150,
                     zIndex: 100,
@@ -58,56 +57,9 @@ BX.ready(function() {
                             id: 'save-btn',
                             className: 'ui-btn ui-btn-success',
                             events: {
-                                click: function() {
-                                    BX.onCustomEvent("onTimeManDayOpen", {});
+                                click: function () {
 
-                                    $('.tm-control-panel__info:not(.tm-control-panel__info_pause) .tm-timer__value-number_hours').html('00');
-                                    $('.tm-control-panel__info:not(.tm-control-panel__info_pause) .tm-timer__value-number_minutes').html('00');
-                                    $('.tm-control-panel__info:not(.tm-control-panel__info_pause) .tm-timer__value-number_seconds').html('00');
                                     this.popupWindow.close();
-                                    const sayHi = () => {
-                                        if (i == 60) {
-                                            i = 0;
-                                            m ++;
-                                        }
-                                        if (m == 60) {
-                                            m = 0;
-                                            h ++;
-                                        }
-                                        if (h == 24) {
-                                            h = 0;
-                                            m = 0;
-                                            i = 0;
-                                        }
-                                        if (i < 10) {
-                                            sec = '0' + i;
-                                        } else {
-                                            sec = i;
-                                        }
-                                        if (m < 10) {
-                                            min = '0' + m;
-                                        } else {
-                                            min = m;
-                                        }
-                                        if (h < 10) {
-                                            let hour = '0' + h;
-                                        } else {
-                                            hour = h;
-                                        }
-
-                                        $('.tm-control-panel__info:not(.tm-control-panel__info_pause) .tm-timer__value-number_seconds').html(sec);
-                                        $('.tm-control-panel__info:not(.tm-control-panel__info_pause) .tm-timer__value-number_minutes').html(min);
-                                        $('.tm-control-panel__info:not(.tm-control-panel__info_pause) .tm-timer__value-number_hours').html(hour);
-
-                                        i++;
-                                    }
-                                    let i = 0;
-                                    let m = 0;
-                                    let h = 0;
-                                    let sec = '00';
-                                    let min = '00';
-                                    let hour = '00';
-                                    const timerId = setInterval(sayHi, 1000);
                                 }
                             }
                         }),
@@ -116,9 +68,9 @@ BX.ready(function() {
                             id: 'copy-btn',
                             className: 'ui-btn ui-btn-primary',
                             events: {
-                                click: function() {
-                                    p.STATE = state;
-                                    console.log(p.STATE);
+                                click: function () {
+                                    //p.STATE = state;
+                                    //console.log(p.STATE);
                                     this.popupWindow.close();
                                 }
                             }
@@ -129,5 +81,5 @@ BX.ready(function() {
                 $('#popup-window-content-popup-message').hide();
             }
         }
-    });
+    })
 });
