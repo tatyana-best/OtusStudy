5bba8327ca10e2b8064dd29d626a451d4238337f
Tatyana Avdeeva <tatyana.best@list.ru>
2025-12-13 07:19:13 +0300

    Correct results

--- local/php_interface/classes/Otus/CRM/Deals/UpdateRequestsIblockAfterDealUpdate.php
+++ local/php_interface/classes/Otus/CRM/Deals/UpdateRequestsIblockAfterDealUpdate.php
@@ -18,68 +18,47 @@ class UpdateRequestsIblockAfterDealUpdate
     public static function updateRequest(&$arFields)
     {
         define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"] . "/local/logs/log.txt");
-
+        
         $elementIdToUpdate = self::findIblockElementIdByDeal($arFields['ID']);
         $iblockId = static::REQUEST_IBLOCK_ID;
 
         if ($elementIdToUpdate) {
             $propertyCode = "AMOUNT";
-            $oldValue = self::getOldAmount($arFields['ID']);
+            $oldValue = self::getOldValue($arFields['ID'], 'OPPORTUNITY');
             $newValue = $arFields['OPPORTUNITY'];
 
-            $error1 = false;
             if ($oldValue != $newValue) {
                 \CIBlockElement::SetPropertyValuesEx($elementIdToUpdate, $iblockId, [$propertyCode => $newValue]);
-            } else {
-                $error1 = true;
             }
 
             $propertyCode = "RESPONSIBLE";
-            $oldValue = self::getOldResponsible($arFields['ID']);
+            $oldValue = self::getOldValue($arFields['ID'], 'ASSIGNED_BY_ID');
             $newValue = $arFields['ASSIGNED_BY_ID'];
 
-            $error2 = false;
             if ($oldValue != $newValue) {
                 \CIBlockElement::SetPropertyValuesEx($elementIdToUpdate, $iblockId, [$propertyCode => $newValue]);
-            } else {
-                $error2 = true;
             }
         } else {
             AddMessage2Log("Такой сделки не существует", "iblock");
             return false;
         }
 
-        return !($error1 && $error2);
+        return true;
     }
 
-    public static function getOldAmount($dealId)
+    public static function getOldValue($dealId, $field)
     {
         $arDeals = DealTable::getList([
             'filter' => ['ID' => $dealId],
-            'select' => ['OPPORTUNITY'],
+            'select' => [$field],
         ])->fetchAll();
 
         $sum = 0;
         foreach($arDeals as $deal){
-            $sum = $deal['OPPORTUNITY'];
+            $sum = $deal[$field];
         }
 
         return $sum;
-    }
-
-    public static function getOldResponsible($dealId)
-    {
-        $arDeals = DealTable::getList([
-            'filter' => ['ID' => $dealId],
-            'select' => ['ASSIGNED_BY_ID'],
-        ])->fetchAll();
-
-        $resopsible = 0;
-        foreach($arDeals as $deal){
-            $resopsible = $deal['ASSIGNED_BY_ID'];
-        }
-
-        return $resopsible;
     }
 
     public static function findIblockElementIdByDeal($dealId)
--- local/php_interface/classes/Otus/IBLOCK/UpdateDealAfterRequestsIblockUpdate.php
+++ local/php_interface/classes/Otus/IBLOCK/UpdateDealAfterRequestsIblockUpdate.php
@@ -2,6 +2,7 @@
 
 namespace Otus\IBLOCK;
 
+use Bitrix\Crm\DealTable;
 use \Bitrix\Main\Context;
 use \Bitrix\Crm\Service\Container;
 use Bitrix\Main\Loader;
@@ -25,83 +26,70 @@ class UpdateDealAfterRequestsIblockUpdate
             $dealId = $val['VALUE'];
         }
 
-        $newAmount = 0;
-        foreach ($arFields['PROPERTY_VALUES'][static::PROPERTY_AMOUNT] as $val) {
-            $newAmount = explode('|', $val['VALUE'])[0];
+        if ($dealId) {
+            $newAmount = 0;
+            foreach ($arFields['PROPERTY_VALUES'][static::PROPERTY_AMOUNT] as $val) {
+                $newAmount = explode('|', $val['VALUE'])[0];
+            }
+
+            $oldAmount = self::getOldValue($arFields['ID'], 'AMOUNT');
+
+            if ($oldAmount != $newAmount || self::getFeildValueFromDeal($dealId, 'OPPORTUNITY') != $newAmount) {
+                self::updateFieldDeal($dealId, 'OPPORTUNITY', $newAmount);
+            }
+
+            $newResponsible = 0;
+            foreach ($arFields['PROPERTY_VALUES'][static::PROPERTY_RESPONSIBLE] as $val) {
+                $newResponsible = $val;
+            }
+
+            $oldResponsible = self::getOldValue($arFields['ID'], 'RESPONSIBLE');
+
+            if ($oldResponsible != $newResponsible || self::getFeildValueFromDeal($dealId, 'ASSIGNED_BY_ID') != $newResponsible) {
+                self::updateFieldDeal($dealId, 'ASSIGNED_BY_ID', $newResponsible);
+            }
         }
 
-        $oldAmount = self::getOldAmount($arFields['ID']);
-
-        $error1 = false;
-        if ($oldAmount != $newAmount && $newAmount > 0) {
-            self::updateAmount($dealId, $newAmount);
-        } else {
-            $error1 = true;
-        }
-
-        $newResponsible = 0;
-        foreach ($arFields['PROPERTY_VALUES'][static::PROPERTY_RESPONSIBLE] as $val) {
-            $newResponsible = $val;
-        }
-
-        $oldResponsible = self::getOldResponsible($arFields['ID']);
-
-        $error2 = false;
-        if ($oldResponsible != $newResponsible) {
-            self::updateResponsible($dealId, $newResponsible);
-        } else {
-            $error2 = true;
-        }
-
-        return !($error1 && $error2);
+        return true;
     }
 
-    public static function getOldAmount($elementId)
+    public static function getOldValue($elementId, $field)
     {
         $iblock = Iblock::wakeUp(static::REQUEST_IBLOCK_ID);
 
         $element = $iblock->getEntityDataClass()::getByPrimary(
             $elementId,
-            ['select' => ['AMOUNT']])
+            ['select' => [$field]])
             ->fetchObject();
 
-        $sum = $element->get('AMOUNT')->getValue();
+        $sum = $element->get($field)->getValue();
         $sum = explode('|', $sum)[0];
 
         return $sum;
     }
 
-    public static function getOldResponsible($elementId)
-    {
-        $iblock = Iblock::wakeUp(static::REQUEST_IBLOCK_ID);
-
-        $element = $iblock->getEntityDataClass()::getByPrimary(
-            $elementId,
-            ['select' => ['RESPONSIBLE']])
-            ->fetchObject();
-
-        $resp = $element->get('RESPONSIBLE')->getValue();
-
-        return $resp;
-    }
-
-    public static function updateResponsible($dealId, $responsible)
+    public static function updateFieldDeal($dealId, $field, $fieldValue)
     {
         $factory = Container::getInstance()->getFactory(2);
         $item = $factory->getItem($dealId);
-        $item->set('ASSIGNED_BY_ID', $responsible);
+        $item->set($field, $fieldValue);
         $item->save();
         $operation = $factory->getUpdateOperation($item);
         $operation->launch();
     }
 
-    public static function updateAmount($dealId, $amount)
+    public static function getFeildValueFromDeal($dealId, $field)
     {
-        $factory = Container::getInstance()->getFactory(2);
-        $item = $factory->getItem($dealId);
-        $item->setOpportunity($amount);
-        $item->save();
-        $operation = $factory->getUpdateOperation($item);
-        $operation->launch();
+        $arDeals = DealTable::getList([
+            'filter' => ['ID' => $dealId],
+            'select' => [$field],
+        ])->fetchAll();
+
+        $sum = 0;
+        foreach($arDeals as $deal){
+            $sum = $deal[$field];
+        }
+
+        return $sum;
     }
 }
